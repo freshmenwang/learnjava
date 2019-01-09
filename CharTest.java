54a8925948f07b0ff16211b08200c7c3642fceb9
chris <1278271343@qq.com>
2019-01-09 23:31:17 +0800

    2019.1.9日记 对一行定义赋值多个变量和赋值中文给char类型变量的测试

---
+++ CharTest.java
@@ -0,0 +1,6 @@
+public class CharTest{
+	public static void main(String[] args){
+		char charTest = '一';
+		System.out.println(charTest);
+	}
+}
---
+++ VarText0.java
@@ -0,0 +1,6 @@
+public class VarText0{
+	public static void main(String[] args){
+		int var1=10,var2=2,var3=3;
+		System.out.println(var1+" "+var2+" "+var3);
+	}
+}
