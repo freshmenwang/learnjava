cddee40de5d781fd62605314572a588a3de74b3f
chris <1278271343@qq.com>
2019-01-11 00:18:34 +0800

    2019.1.10的日记和转换规则还有一些小测试。

---
+++ FloatTest.java
@@ -0,0 +1,6 @@
+public class FloatTest{
+	public static void main(String[] args){
+		float f = 4.1f;
+		System.out.println(f);
+	}
+}
---
+++ VarTest1.java
@@ -0,0 +1,6 @@
+public class VarTest1{
+	public static void main(String[] args){
+		long a = 0xabc12345678L;
+		System.out.println(a+"\"");
+	}
+}
