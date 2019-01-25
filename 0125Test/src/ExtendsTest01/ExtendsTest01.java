15353a6e58a0fa8bee8085e0215dec721f8199af
chris <1278271343@qq.com>
2019-01-25 23:56:36 +0800

    1.25日记和继承与static的一些练习

---
+++ 0125Test/src/ExtendsTest01/ExtendsTest01.java
@@ -0,0 +1,16 @@
+package ExtendsTest01;
+
+public class ExtendsTest01{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student("三年级","二十五小");
+		s.setAge(10);
+		Student n = new Student();
+		System.out.println(s.getAge()+n.getSchool()+n.getGrade());
+	}
+
+}
---
+++ 0125Test/src/StaticTest01/StaticTest01.java
@@ -0,0 +1,21 @@
+package StaticTest01;
+
+public class StaticTest01 {
+
+	/**
+	 * @param args
+	 * 
+	 */
+	static{
+		System.out.println("cs----1");
+	}
+	static{
+		System.out.println("cs----2");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main run");
+	}
+
+}
