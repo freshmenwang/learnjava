10011ec342d3fb1fba7421f2fe6b18d01488d91e
chris <1278271343@qq.com>
2019-02-02 23:13:02 +0800

    2.2日记和对单例模式的练习和了解。

---
+++ 0202Test/src/singletontest/Singleton01.java
@@ -0,0 +1,15 @@
+package singletontest;
+/**
+ * ÀÁººµ¥Àý
+*/
+public class Singleton01 {
+	private static Singleton01 s = null;
+	private Singleton01(){
+	}
+	public static Singleton01 getSingleton01(){
+		if(s == null){
+			s = new Singleton01();
+		}
+		return s;
+	}
+}
---
+++ 0202Test/src/singletontest/SingletonTest.java
@@ -0,0 +1,19 @@
+package singletontest;
+
+public class SingletonTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Singleton s1 = Singleton.getSingleton();
+		Singleton s2 = Singleton.getSingleton();
+		System.out.println(s1 == s2);
+		System.out.println("-----------------");
+		Singleton01 s3 = Singleton01.getSingleton01();
+		Singleton01 s4 = Singleton01.getSingleton01();
+		System.out.println(s3 == s4);
+	}
+
+}
